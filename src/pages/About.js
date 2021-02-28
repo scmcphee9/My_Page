2b69c12513e2503c8f58c50fe36dec2426c23e80
Shawn McPhee <scmcphee9@gmail.com>
2021-02-28 10:58:22 -0700

    Images imported and now displaying  properly

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,44 +1,45 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import Photo from "./assets/images/IMG_1463.jpg";
 
 function About() {
   return (
     <Card style={{ width: "75%" }}>
-      <Card.Img
-        variant="top"
-        src="./assets/image/IMG_1463.jpg"
-        alt="Head Shot"
-      />
-      <Card.Body>
-        <Card.Title>About Me</Card.Title>
-        <Card.Text>
-          I am a full stack web developer, utilizing my engineering and sales
-          background, to build a great user experience on the web. I recently
-          earned a certificate in full stack web development from the University
-          of Denver, with newly developed skills in JavasScript, CSS, React.js,
-          Node. I enjoy making a website function as it should by ensuring the
-          front end and back end are communicating properly to maintain a high
-          level of client satisfaction. My experience with building applications
-          has been fun, challenging, and exciting as I can put my skills to the
-          test and continually develop them with every project. I am a creative
-          problem solver, quick learner, and a great communicator. I look
-          forward to contributing to a team that demonstrates a passion for
-          teamwork and achievement through a commitment to positive work
-          environments and diversity of thought.
-        </Card.Text>
-        <Card.Text>
-          I am from the east coast: Massachusetts, Maine, and Pennsylvannia. I
-          moved out to Colorado in 2019, to a small ski town in Steamboat
-          Springs. I have been married for 5 years to my ever supporting wife
-          Sarah, and together we have a two year old son William. We also have a
-          German Shephard and two cats. My interests are in the outdoors, this
-          includes hiking, biking, skiing, scuba diving, etc. Photography is
-          something I have recently gained a passion for. In particular night
-          photography and macro. If you want to see some of my work and
-          experience check out my portfolio, resume, or links to my github and
-          linkedIn.
-        </Card.Text>
-      </Card.Body>
+      <div className="row no-gutters">
+        <Card style={{ width: "25%", float: "left" }}>
+          <Card.Img src={Photo} alt="Head Shot" />
+        </Card>
+        <Card.Body>
+          <Card.Title>About Me</Card.Title>
+          <Card.Text>
+            I am a full stack web developer, utilizing my engineering and sales
+            background, to build a great user experience on the web. I recently
+            earned a certificate in full stack web development from the
+            University of Denver, with newly developed skills in JavasScript,
+            CSS, React.js, Node. I enjoy making a website function as it should
+            by ensuring the front end and back end are communicating properly to
+            maintain a high level of client satisfaction. My experience with
+            building applications has been fun, challenging, and exciting as I
+            can put my skills to the test and continually develop them with
+            every project. I am a creative problem solver, quick learner, and a
+            great communicator. I look forward to contributing to a team that
+            demonstrates a passion for teamwork and achievement through a
+            commitment to positive work environments and diversity of thought.
+          </Card.Text>
+          <Card.Text>
+            I am from the east coast: Massachusetts, Maine, and Pennsylvannia. I
+            moved out to Colorado in 2019, to a small ski town in Steamboat
+            Springs. I have been married for 5 years to my ever supporting wife
+            Sarah, and together we have a two year old son William. We also have
+            a German Shephard and two cats. My interests are in the outdoors,
+            this includes hiking, biking, skiing, scuba diving, etc. Photography
+            is something I have recently gained a passion for. In particular
+            night photography and macro. If you want to see some of my work and
+            experience check out my portfolio, resume, or links to my github and
+            linkedIn.
+          </Card.Text>
+        </Card.Body>
+      </div>
     </Card>
   );
 }
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,12 @@
 import React from "react";
 import CardData from "../components/Card/CardData";
-
 import { CardDeck } from "react-bootstrap";
+import KeepDreaming from "./assets/images/Keep_Dreaming.png";
+import EmployeeDirectory from "./assets/images/employee-directory.png";
+import TreePic from "./assets/images/treeQuiz.png";
+import FitnessPic from "./assets/images/fitnessTracker.png";
+import Weather from "./assets/images/WeatherApp.png";
+import NoteTaker from "./assets/images/Note_Taker.png";
 
 function Portfolio() {
   const portfolioArray = [
@@ -11,7 +16,7 @@ function Portfolio() {
         "Select you travel destination by answering some basic questions, and a list of locations will be displayed.  This project is currently only using seeded data so not all options will be displayed.  This is a full stack project utilizing mySQL, express, node, and javascript",
       repo: "https://github.com/scmcphee9/Keep_Dreaming",
       deployed: "https://frozen-journey-37808.herokuapp.com/",
-      image: "./assets/images/Keep_Dreaming.png",
+      image: KeepDreaming,
     },
     {
       title: "Employee Directory",
@@ -19,7 +24,7 @@ function Portfolio() {
         "This is an application that can hold all of your employees. You can search by their name or phone number to sort through the list. You can also order the list in alphabetical order, ascending or descending. This application uses react.",
       repo: "https://github.com/scmcphee9/employee_directory",
       deployed: "https://pensive-snyder-69b1b3.netlify.app/",
-      image: "./assets/images/employee-directory.png",
+      image: EmployeeDirectory,
     },
     {
       title: "Tree Quiz",
@@ -27,30 +32,39 @@ function Portfolio() {
         "Test your knoweledge on trees in the one minute quiz! See if you can set a High Score.  This application uses JQuery and Template literals to iterate through the questions.",
       repo: "https://github.com/scmcphee9/Timed-Quiz",
       deployed: "https://scmcphee9.github.io/Timed-Quiz/",
-      image: "./assets/images/treeQuiz.png",
+      image: TreePic,
     },
     {
-      title: "Tree Quiz",
+      title: "Fitness Tracker",
       description:
-        "Test your knoweledge on trees in the one minute quiz! See if you can set a High Score.  This application uses JQuery and Template literals to iterate through the questions.",
-      repo: "https://github.com/scmcphee9/Timed-Quiz",
-      deployed: "https://scmcphee9.github.io/Timed-Quiz/",
-      image: "./assets/images/treeQuiz.png",
+        "Track your fitness days and workouts by cardio or weight lifting.  This app will show you your past workout information and charts on progress.  This app uses",
+      repo: "https://github.com/scmcphee9/Workout_tracker",
+      deployed:
+        "https://secure-basin-67312.herokuapp.com/?id=6023329785d2e4001182ad79",
+      image: FitnessPic,
     },
     {
-      title: "Tree Quiz",
+      title: "Weather Application",
       description:
-        "Test your knoweledge on trees in the one minute quiz! See if you can set a High Score.  This application uses JQuery and Template literals to iterate through the questions.",
-      repo: "https://github.com/scmcphee9/Timed-Quiz",
-      deployed: "https://scmcphee9.github.io/Timed-Quiz/",
-      image: "./assets/images/treeQuiz.png",
+        "Going on a trip? Check out the weather of where you are going and the long range forecast to help predict what to bring and wear.",
+      repo: "https://github.com/scmcphee9/weather_app",
+      deployed: "https://scmcphee9.github.io/weather_app/",
+      image: Weather,
+    },
+    {
+      title: "Note Taker",
+      description:
+        "Need to take note or make a list? This app does that! Take notes, save them, and remove them when they are complete!",
+      repo: "https://github.com/scmcphee9/Note_Taker",
+      deployed: "https://afternoon-dawn-66402.herokuapp.com/",
+      image: NoteTaker,
     },
   ];
 
   return (
     <>
       <h3>Portfolio</h3>
-      {/* <div className="card mb-4"> */}
+
       <CardDeck>
         {portfolioArray.map((item) => (
           <CardData
@@ -63,7 +77,6 @@ function Portfolio() {
           />
         ))}
       </CardDeck>
-      {/* </div> */}
     </>
   );
 }
