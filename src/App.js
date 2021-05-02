2183e9c32e7ecf030770193ccf1498c1f1781e35
Shawn McPhee <scmcphee9@gmail.com>
2021-05-02 16:02:52 -0600

    added resume to resume page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ContactMe from "./pages/ContactMe";
 import Resume from "./pages/Resume";
 import MyNavbar from "./components/Navbar";
 import Footer from "./components/Footer/Footer";
+import pdf from "./pages/assets/images/Shawn McPhee Website.pdf";
 
 function App() {
   return (
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { Document, Page } from "react-pdf";
+import pdf from "./assets/images/Shawn McPhee Website.pdf";
+import "./Resume.css";
+
+const url =
+  "https://drive.google.com/file/d/1s4PLRLRTepvnY-Mb5Gt9ouUv0LtN6mEH/view";
 
 function Resume() {
+  // const [numPages, setNumPages] = useState(null);
+  // const [pageNumber, setPageNumber] = useState(1);
+
+  // function onDocumentLoadSuccess({ numPages }) {
+  //   setNumPages(numPages);
+  // }
+  // function onResumeClick() {
+  //   window.open(pdf);
+  // }
+  // const { pdf } = props;
+
   return (
-    <div>
-      <iframe
-        src="https://docs.google.com/gview?url=https://drive.google.com/file/d/1s4PLRLRTepvnY-Mb5Gt9ouUv0LtN6mEH/view&embedded=true"
-        style="width:600px; height:500px;"
-        frameborder="0"
-      ></iframe>
-    </div>
+    <iframe
+      className="resume"
+      src="https://docs.google.com/gview?url=https://github.com/scmcphee9/resume/raw/main/ShawnMcPheeWebsite.pdf&embedded=true"
+      // style="width: 100%; height: 100%"
+      // frameborder="0"
+    ></iframe>
+    // <a onClick={onResumeClick}>Resume</a>
+    // <div>
+    //   <a
+    //     href={require("./assets/images/Shawn McPhee Website.pdf")}
+    //     target="_blank"
+    //   ></a>
+    // </div>
+    // <div>
+    //   <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+    //     <Page pageNumber={pageNumber} />
+    //   </Document>
+    //   <p>
+    //     Page {pageNumber} of {numPages}
+    //   </p>
+    // </div>
   );
 }
 
